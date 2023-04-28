c09971be2734ce793e8ba467585f098587612450
vozmi <mvozbranuk@gmail.com>
2023-04-28 11:05:39 +0300

    configure routing

--- www/src/App.tsx
+++ www/src/App.tsx
@@ -1,5 +1,15 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { StoriesList } from "@/pages";
+
 const App = () => {
-    return <div>App</div>;
+    return (
+        <Routes>
+            <Route path="/news" element={<StoriesList />}>
+                <Route path=":id" element={<StoriesList />} />
+            </Route>
+            <Route path="*" element={<Navigate to="/news" />} />
+        </Routes>
+    );
 };
 
 export default App;
