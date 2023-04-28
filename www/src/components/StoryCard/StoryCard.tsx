4129f3d5d7cba51d926ca5878890732bf12ec239
vozmi <mvozbranuk@gmail.com>
2023-04-28 17:47:11 +0300

    add main params to StoryCard

--- www/src/components/StoryCard/StoryCard.tsx
+++ www/src/components/StoryCard/StoryCard.tsx
@@ -1,9 +1,33 @@
 import { Story } from "@/models";
+import { Card, CardContent } from "@mui/material";
 
 type Props = {
     data: Story;
 };
 
 export const StoryCard: React.FC<Props> = ({ data }) => {
-    return <div data-testid="storycard">{data.title}</div>;
+    return (
+        <div data-testid="storycard">
+            <Card sx={{ maxWidth: 500 }}>
+                <CardContent data-testid="storycard__title">
+                    {data.title}
+                </CardContent>
+                <CardContent data-testid="storycard__author">
+                    {data.author}
+                </CardContent>
+                <CardContent data-testid="storycard__url">
+                    {data.url}
+                </CardContent>
+                <CardContent data-testid="storycard__commentsCount">
+                    {data.allCommentsCount}
+                </CardContent>
+                <CardContent data-testid="storycard__createDate">
+                    {data.createDate}
+                </CardContent>
+                <CardContent data-testid="storycard__score">
+                    {data.score}
+                </CardContent>
+            </Card>
+        </div>
+    );
 };
--- www/src/components/StoryCard/__tests__/StoryCard.test.tsx
+++ www/src/components/StoryCard/__tests__/StoryCard.test.tsx
@@ -3,19 +3,50 @@ import { StoryCard } from "../StoryCard";
 import { ApiAdapter } from "@/services";
 import { ApiClient } from "@/api";
 import { MOCK_SERVER_URL, MOCK_STORY_ID } from "@/mocks";
+import { Story } from "@/models";
+
+const story: Story = {
+    id: 48765231,
+    url: "http://www.getdropbox.com/u/2/screencast.html",
+    title: "My YC app: Dropbox - Throw away your USB drive",
+    score: 111,
+    author: "dhouston",
+    createDate: "1/14/1970",
+    allCommentsCount: 71,
+    childCommentIds: [32456782, 32456782, 32456782],
+};
+
+// storycard__title
+// storycard__author
+// storycard__url
+// storycard__commentsCount
+// storycard__createDate
+// storycard__score
 
 describe("StoryCard", () => {
-    it("Renders correctly", async () => {
-        const apiAdapter = new ApiAdapter(
-            new ApiClient({ base: MOCK_SERVER_URL })
-        );
-
-        const data = await apiAdapter.getStory(MOCK_STORY_ID);
-
-        render(<StoryCard data={data} />);
+    it("Should show main variables (title, author, commentsCount, score, createDate)", async () => {
+        render(<StoryCard data={story} />);
 
         await screen.findByTestId("storycard");
 
-        expect(screen.getByTestId("storycard")).toHaveTextContent(data.title);
+        expect(screen.getByTestId("storycard__title")).toHaveTextContent(
+            story.title
+        );
+
+        expect(screen.getByTestId("storycard__author")).toHaveTextContent(
+            story.author
+        );
+
+        expect(
+            screen.getByTestId("storycard__commentsCount")
+        ).toHaveTextContent(story.allCommentsCount.toString());
+
+        expect(screen.getByTestId("storycard__score")).toHaveTextContent(
+            story.score.toString()
+        );
+
+        expect(screen.getByTestId("storycard__createDate")).toHaveTextContent(
+            story.createDate
+        );
     });
 });
