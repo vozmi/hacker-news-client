24a3164f8fbce006be88e89964cef5c26dfe24d3
vozmi <mvozbranuk@gmail.com>
2023-04-28 17:36:49 +0300

    add @mui/types and App.css

--- www/src/index.tsx
+++ www/src/index.tsx
@@ -2,16 +2,17 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./App.css";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
 );
