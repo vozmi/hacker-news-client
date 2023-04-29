a0819927dd5dc10543ed40198802a9645fda809d
vozmi <mvozbranuk@gmail.com>
2023-04-29 20:10:42 +0300

    refactor tests

--- www/src/pages/StoriesList/components/StoryCardContainer/__tests__/StoryCardContainer.test.tsx
+++ www/src/pages/StoriesList/components/StoryCardContainer/__tests__/StoryCardContainer.test.tsx
@@ -6,28 +6,24 @@ import { ApiAdapter } from "@/services";
 import { queryByLabelText, screen, waitFor } from "@testing-library/react";
 import { StoryCardContainer } from "../StoryCardContainer";
 
-describe("StoryCardContainer", () => {
-    const mockApiAdapter = new ApiAdapter(
-        new ApiClient({ base: MOCK_SERVER_URL })
+const mockApiAdapter = new ApiAdapter(new ApiClient({ base: MOCK_SERVER_URL }));
+
+const renderWithAdapter = (storyId: number) => {
+    return renderWithRouter(
+        <ServicesContext.Provider value={{ apiAdapter: mockApiAdapter }}>
+            <StoryCardContainer id={storyId} />
+        </ServicesContext.Provider>
     );
+};
 
-    const renderWithAdapter = (storyId: number) => {
-        return renderWithRouter(
-            <ServicesContext.Provider value={{ apiAdapter: mockApiAdapter }}>
-                <StoryCardContainer id={storyId} />
-            </ServicesContext.Provider>
-        );
-    };
+test("Should show Skeleton during loading", async () => {
+    renderWithAdapter(MOCK_STORY_ID);
 
-    it("Should show Skeleton during loading", async () => {
-        renderWithAdapter(MOCK_STORY_ID);
+    expect(screen.getByLabelText("skeleton")).toBeInTheDocument();
 
-        expect(screen.getByLabelText("skeleton")).toBeInTheDocument();
-
-        await waitFor(() => {
-            expect(screen.queryByLabelText("skeleton")).not.toBeInTheDocument();
-        });
-
-        expect(screen.getByLabelText("story")).toBeInTheDocument();
+    await waitFor(() => {
+        expect(screen.queryByLabelText("skeleton")).not.toBeInTheDocument();
     });
+
+    expect(screen.getByLabelText("story")).toBeInTheDocument();
 });
