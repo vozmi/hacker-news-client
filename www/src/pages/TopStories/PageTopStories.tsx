def207ceb5c4690004f095cc64669c30a07da005
vozmi <mvozbranuk@gmail.com>
2023-04-29 23:02:23 +0300

    Add content to PageTopStories

--- www/src/pages/TopStories/PageTopStories.tsx
+++ www/src/pages/TopStories/PageTopStories.tsx
@@ -1,3 +1,51 @@
+import { StoryCard } from "@/components";
+import { useServices } from "@/contexts";
+import { useEffect, useState } from "react";
+import { StoryCardContainer } from "./components";
+import { Stack } from "@mui/material";
+import { range } from "@/lib/range";
+
 export const PageTopStories = () => {
-    return <div>PageTopStories</div>;
+    const [isLoading, setLoading] = useState(false);
+    const [topStoriesIds, setTopStoriesIds] = useState<number[]>([]);
+    const { apiAdapter } = useServices();
+
+    const getData = async () => {
+        setLoading(true);
+
+        const topStoriesData = await apiAdapter.getTopStories();
+
+        // show top 200 stories
+        setTopStoriesIds(topStoriesData.slice(0, 200));
+
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        getData();
+
+        return () => {
+            setTopStoriesIds([]);
+        };
+    }, []);
+
+    return (
+        <div>
+            <Stack spacing={2}>
+                {isLoading ? (
+                    <>
+                        {range(0, 200).map((i) => (
+                            <StoryCard.Skeleton key={i} />
+                        ))}
+                    </>
+                ) : (
+                    <>
+                        {topStoriesIds.map((storyId) => (
+                            <StoryCardContainer key={storyId} id={storyId} />
+                        ))}
+                    </>
+                )}
+            </Stack>
+        </div>
+    );
 };
