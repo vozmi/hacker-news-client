a9db2c7a22bab6e40169bc40e57ba8278b5304e6
vozmi <mvozbranuk@gmail.com>
2023-04-29 13:35:10 +0300

    change navigation to click on card instead of link

--- www/src/components/StoryCard/StoryCard.tsx
+++ www/src/components/StoryCard/StoryCard.tsx
@@ -1,26 +1,34 @@
 import { Story } from "@/models";
 import { Card, CardContent, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 type Props = {
     data: Story;
 };
 
 export const StoryCard: React.FC<Props> = ({ data }) => {
+    const navigate = useNavigate();
+
+    const goToStoryPage = () => {
+        navigate(`/news/${data.id}`);
+    };
+
     return (
-        <div data-testid="storycard">
-            <Card sx={{ maxWidth: 500 }}>
-                <CardContent>
-                    <Typography sx={{ fontSize: 14 }}>{data.title}</Typography>
-                    <Typography sx={{ fontSize: 10 }}>
-                        {data.score} points | by {data.author} |{" "}
-                        {data.createDate} | {data.allCommentsCount} comments
-                    </Typography>
-                </CardContent>
-                <CardContent>
-                    <Link to={`/news/${data.id}`}>Read more</Link>
-                </CardContent>
-            </Card>
-        </div>
+        <Card
+            data-testid="storycard"
+            sx={{ maxWidth: 500, cursor: "pointer" }}
+            onClick={goToStoryPage}
+        >
+            <CardContent>
+                <Typography sx={{ fontSize: 14 }}>{data.title}</Typography>
+                <Typography sx={{ fontSize: 10 }}>
+                    {data.score} points | by {data.author} | {data.createDate} |{" "}
+                    {data.allCommentsCount} comments
+                </Typography>
+            </CardContent>
+            <CardContent>
+                <Link to={`/news/${data.id}`}>Read more</Link>
+            </CardContent>
+        </Card>
     );
 };
