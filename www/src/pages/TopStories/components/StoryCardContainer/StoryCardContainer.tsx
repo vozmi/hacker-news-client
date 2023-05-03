f4cdbdc0ae6c2b6f413306d0a294055ad1a09d55
vozmi <mvozbranuk@gmail.com>
2023-05-03 18:04:37 +0300

    change StoryCard data loading (with intersectionObserver)

--- www/src/pages/TopStories/components/StoryCardContainer/StoryCardContainer.tsx
+++ www/src/pages/TopStories/components/StoryCardContainer/StoryCardContainer.tsx
@@ -1,7 +1,8 @@
 import { StoryCard } from "@/components";
 import { useServices } from "@/contexts";
+import { observeRootIntersection } from "@/lib/rootIntersectionObserver";
 import { Story } from "@/models";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Props = {
     id: number;
@@ -10,6 +11,7 @@ type Props = {
 export const StoryCardContainer: React.FC<Props> = ({ id }) => {
     const [isLoading, setLoading] = useState(false);
     const [data, setData] = useState<Story | null>(null);
+    const containerRef = useRef<HTMLDivElement>(null);
 
     const { apiAdapter } = useServices();
 
@@ -19,25 +21,44 @@ export const StoryCardContainer: React.FC<Props> = ({ id }) => {
             const story = await apiAdapter.getStory(id);
             setData(story);
         } catch (error: any) {
-            console.error(
-                "Error during getting StoryCard data: " + error.message
-            );
+            console.error("Error while loading StoryCard data:", error.message);
         } finally {
             setLoading(false);
         }
     };
 
     useEffect(() => {
-        getData();
+        if (containerRef.current !== null) {
+            // Observe current component
+            const disposeObserver = observeRootIntersection(
+                containerRef.current,
+                async (entry) => {
+                    if (entry.intersectionRatio > 0 && !data && !isLoading) {
+                        // Load data when intersects
+                        await getData();
 
-        return () => {
+                        // Remove observer after loading data
+                        disposeObserver();
+                    }
+                }
+            );
+        }
+    }, [containerRef]);
+
+    useEffect(() => {
+        return function cleanup() {
             setData(null);
+            setLoading(false);
         };
     }, []);
 
-    if (isLoading || !data) {
-        return <StoryCard.Skeleton />;
-    }
-
-    return <StoryCard data={data} />;
+    return (
+        <div ref={containerRef} style={{ width: 500 }}>
+            {isLoading || data === null ? (
+                <StoryCard.Skeleton />
+            ) : (
+                <StoryCard data={data} />
+            )}
+        </div>
+    );
 };
