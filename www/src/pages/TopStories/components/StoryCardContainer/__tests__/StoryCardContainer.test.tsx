1db6ae28e45b4d19f91a8465fc97eaab7a29edc4
vozmi <mvozbranuk@gmail.com>
2023-05-04 15:50:13 +0300

    fix StoryCardContainer test

--- www/src/pages/TopStories/components/StoryCardContainer/__tests__/StoryCardContainer.test.tsx
+++ www/src/pages/TopStories/components/StoryCardContainer/__tests__/StoryCardContainer.test.tsx
@@ -8,13 +8,39 @@ import { StoryCardContainer } from "../StoryCardContainer";
 
 const mockApiAdapter = new ApiAdapter(new ApiClient({ base: MOCK_SERVER_URL }));
 
+const mockDomRect: DOMRectReadOnly = {
+    x: 100,
+    y: 100,
+    top: 100,
+    right: 100,
+    bottom: 100,
+    left: 100,
+    width: 100,
+    height: 100,
+    toJSON: jest.fn(),
+};
+
 const renderWithAdapter = (storyId: number) => {
     return renderWithRouter(
         <ServicesContext.Provider
             value={{
                 apiAdapter: mockApiAdapter,
                 rootIntersectionObserver: {
-                    observe: jest.fn(),
+                    observe: (el, callback) => {
+                        callback({
+                            target: el,
+                            isIntersecting: true,
+                            intersectionRatio: 1,
+                            boundingClientRect: mockDomRect,
+                            intersectionRect: mockDomRect,
+                            rootBounds: mockDomRect,
+                            time: 1,
+                        });
+
+                        return () => {
+                            // dispose
+                        };
+                    },
                     dispose: jest.fn(),
                 },
             }}
