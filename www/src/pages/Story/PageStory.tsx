3fb5b0e4e7dfd92b665c0fdb0d5c6012c799c720
vozmi <mvozbranuk@gmail.com>
2023-05-10 15:13:45 +0300

    add basic PageStory

--- www/src/pages/Story/PageStory.tsx
+++ www/src/pages/Story/PageStory.tsx
@@ -1,3 +1,26 @@
+import { useParams } from "react-router-dom";
+import { CommentTree } from "./components";
+import { useEffect, useState } from "react";
+import { Story } from "@/models";
+import { useServices } from "@/contexts";
+
 export const PageStory = () => {
-    return <div>PageStory</div>;
+    const { id } = useParams();
+    const [story, setStory] = useState<Story>();
+    const { apiAdapter } = useServices();
+
+    useEffect(() => {
+        if (id) {
+            apiAdapter.getStory(+id).then((data) => setStory(data));
+        }
+    }, []);
+
+    return (
+        <div>
+            {story?.childCommentIds &&
+                story.childCommentIds.map((id) => (
+                    <CommentTree id={id} key={id} />
+                ))}
+        </div>
+    );
 };
