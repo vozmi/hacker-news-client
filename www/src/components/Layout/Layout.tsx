96c9328b2aca51727c088a2772eeb43b4cd76ca8
vozmi <mvozbranuk@gmail.com>
2023-05-29 03:15:45 +0300

    blured background on scroll

--- www/src/components/Layout/Layout.tsx
+++ www/src/components/Layout/Layout.tsx
@@ -1,13 +1,39 @@
-import { Header } from "./components";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./Layout.module.scss";
+import { Header } from "./components";
 
 export const Layout: React.FC<{
     children?: string | JSX.Element | JSX.Element[];
 }> = ({ children }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const [isHeaderBlured, setHeaderBlured] = useState(false);
+
+    const scrollEvListener = useCallback(() => {
+        if (
+            !isHeaderBlured &&
+            ref.current?.scrollTop &&
+            ref.current.scrollTop > 64
+        ) {
+            setHeaderBlured(true);
+        } else {
+            setHeaderBlured(false);
+        }
+    }, [setHeaderBlured]);
+
+    useEffect(() => {
+        ref.current?.addEventListener("scroll", scrollEvListener);
+
+        return () => {
+            ref.current?.removeEventListener("scroll", scrollEvListener);
+        };
+    }, [ref]);
+
     return (
-        <div className={styles.container}>
-            <Header />
-            <main className={styles.content}>{children}</main>
+        <div ref={ref} className={styles.container}>
+            <Header blured={isHeaderBlured} />
+            <main id="layout-content" className={styles.content}>
+                {children}
+            </main>
         </div>
     );
 };
